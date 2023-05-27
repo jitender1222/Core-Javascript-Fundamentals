0dcff85702c7876f654e3b5d936e913e1aee5c6b
jitender singh <jeetusinghkanyal@gmail.com>
2023-05-27 19:28:35 +0530

    another way to write this code

--- Recursion/sum.js
+++ Recursion/sum.js
@@ -1,12 +1,15 @@
 // Q1 give me a sum of sum(1)(2)(3)(4) ..... () -> 10
 
-const sum = (a) => {
-  return (b) => {
-    if (b !== undefined) {
-      return sum(a + b);
-    }
-    return a;
-  };
-};
+// const sum = (a) => {
+//   return (b) => {
+//     if (b !== undefined) {
+//       return sum(a + b);
+//     }
+//     return a;
+//   };
+// };
 
+// console.log(sum(1)(2)(0)(4)());
+
+const sum = (a) => (b) => b !== undefined ? sum(a + b) : a;
 console.log(sum(1)(2)(0)(4)());
