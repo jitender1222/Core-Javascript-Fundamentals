30a896122ee2974c2d366130048fa4fcd76fb02d
jitender singh <jeetusinghkanyal@gmail.com>
2023-06-28 11:39:08 +0530

    completed promise pollyfill

--- Polyfill/promises.js
+++ Polyfill/promises.js
@@ -1,33 +1,71 @@
 //  Write a polyfill for a promise
 
 function PromisePolyFill(executor) {
-  let onResolve, onReject;
+  let onResolve,
+    onReject,
+    isFullfilled = false,
+    isRejected = false,
+    iscalled = false,
+    value;
 
-  function resolve(value) {
-    onResolve(value);
+  console.log("resolveeee");
+  function resolve(val) {
+    isFullfilled = true;
+    value = val;
+
+    if (typeof onResolve === "function") {
+      onResolve(val);
+      iscalled = true;
+    }
   }
 
-  function reject(value) {
-    onReject(value);
+  function reject(val) {
+    isRejected = true;
+    value = val;
+
+    if (typeof onResolve === "function") {
+      onReject(val);
+      iscalled = true;
+    }
+    onReject(val);
   }
 
+  // resolve
   this.then = function (callback) {
     onResolve = callback;
+
+    if (isFullfilled && !iscalled) {
+      iscalled = true;
+      onResolve(value);
+    }
     return this;
   };
 
+  // reject
   this.catch = function (callback) {
     onReject = callback;
+
+    if (isRejected && !iscalled) {
+      iscalled = true;
+      onReject(value);
+    }
     return this;
   };
 
-  executor(resolve, reject);
+  try {
+    executor(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
 }
 
+// polyfill
 const examplePolyfill = new PromisePolyFill((resolve, reject) => {
   setTimeout(() => {
     resolve("It will be executed");
   }, 1000);
+
+  // resolve("2");
 });
 
 examplePolyfill
@@ -37,3 +75,19 @@ examplePolyfill
   .catch((error) => {
     console.log(error);
   });
+
+// polyfill for resolve
+
+PromisePolyFill.resolve = (val) => {
+  return new PromisePolyFill(function executor(resolve, reject) {
+    resolve(val);
+  });
+};
+
+// polyfill for reject
+
+PromisePolyFill.reject = (val) => {
+  return new PromisePolyFill(function executor(resolve, reject) {
+    reject(val);
+  });
+};
