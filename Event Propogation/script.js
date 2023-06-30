3f04faa6193e4c52cb88a69293d8fe3abb5a3904
jitender singh <jeetusinghkanyal@gmail.com>
2023-06-30 11:01:30 +0530

    added event bubbling

--- Event Propogation/script.js
+++ Event Propogation/script.js
@@ -2,21 +2,35 @@ const div = document.getElementById("div");
 const form = document.getElementById("form");
 const button = document.getElementById("button");
 
+// event Propogation
+
+div.addEventListener("click", function () {
+  alert("div Triggered");
+});
+
+form.addEventListener("click", function () {
+  alert("form Triggered");
+});
+
+button.addEventListener("click", function () {
+  alert("button Triggered");
+});
+
 // event target vs this.tagName vs currentTarget
 
-div.addEventListener("click", func);
+// div.addEventListener("click", func);
 
-form.addEventListener("click", func);
+// form.addEventListener("click", func);
 
-button.addEventListener("click", func);
+// button.addEventListener("click", func);
 
-function func(event) {
-  alert(
-    "Current Target =" +
-      event.currentTarget.tagName +
-      ", target =" +
-      event.target.tagName +
-      ", this =" +
-      this.tagName
-  );
-}
+// function func(event) {
+//   alert(
+//     "Current Target =" +
+//       event.currentTarget.tagName +
+//       ", target =" +
+//       event.target.tagName +
+//       ", this =" +
+//       this.tagName
+//   );
+// }
